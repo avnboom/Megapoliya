566aaad8348d1cf1a999036dffd8f77392ec73b4
Назаров Алексей <anazarov94@gmail.com>
2019-11-07 15:17:44 +0700

    8

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailsComponent } from './components/heroes/hero-details/hero-details.component';
+import { MessageComponent } from './components/message/message.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailsComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
